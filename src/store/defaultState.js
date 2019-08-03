cc3d5b563193f0c5b1a3a3d26d04ee181e92e6af
Raymond Chen <rchen1992@gmail.com>
2019-08-02 21:36:25 -0700

    Clean up default state.

--- src/store/defaultState.js
+++ src/store/defaultState.js
@@ -1,5 +1,4 @@
 import { getRandomNewBlock } from 'blocks';
-import getBlockCellCoordinateSet from 'blocks/cellCoordinateSet';
 import { createGrid } from 'grid';
 import GAME_STATES from 'constants/gameStates';
 import GAME_SPEEDS from 'constants/gameSpeeds';
@@ -9,18 +8,12 @@ export const GRID_HEIGHT = 20;
 
 export const STARTING_BLOCK_COORDINATES = [0, 3];
 
-const startingBlock = getRandomNewBlock();
-
 export default {
     grid: createGrid(GRID_HEIGHT, GRID_WIDTH),
     currentBlock: {
-        properties: startingBlock,
+        properties: getRandomNewBlock(),
         positionCoordinates: STARTING_BLOCK_COORDINATES,
     },
-    currentBlockCellCoordinateSet: getBlockCellCoordinateSet(
-        startingBlock.shape,
-        STARTING_BLOCK_COORDINATES
-    ),
     gameSpeed: GAME_SPEEDS.STARTING,
     gameState: GAME_STATES.NEW_GAME,
 };
