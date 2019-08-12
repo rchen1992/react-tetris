d7b170a3e72e8ef096b0d8b3e63deb0c5c1914dc
Raymond Chen <rchen1992@gmail.com>
2019-08-11 21:49:34 -0700

    Not-so-random color palette picking.

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,10 +2,32 @@ import BaseTheme from './BaseTheme';
 import colorPalettes, { COLOR_PALETTE_KEYS } from './colorPalettes';
 import blockBorders, { BLOCK_BORDER_KEYS } from './blockStyles/border';
 import blockBorderRadiuses, { BLOCK_BORDER_RADIUS_KEYS } from './blockStyles/borderRadius';
+import { shuffle } from 'lodash';
+
+let unseenColorPalettes = getShuffledColorPalettesKeys();
 
 function getRandomObjectKey(obj) {
     const objKeys = Object.keys(obj);
     return objKeys[Math.floor(Math.random() * objKeys.length)];
+}
+
+function getShuffledColorPalettesKeys() {
+    return [...shuffle(Object.keys(colorPalettes))];
+}
+
+/**
+ * Not really random because we want each theme
+ * to be shown once before being randomly shuffled again.
+ */
+function getRandomColorPaletteKey() {
+    if (unseenColorPalettes.length === 0) {
+        unseenColorPalettes = getShuffledColorPalettesKeys();
+    }
+
+    const palette = unseenColorPalettes[0];
+    unseenColorPalettes = unseenColorPalettes.slice(1);
+
+    return palette;
 }
 
 export function getTheme(colorPalette, blockBorder, blockBorderRadius) {
@@ -20,7 +42,7 @@ export function getTheme(colorPalette, blockBorder, blockBorderRadius) {
 }
 
 export function getRandomTheme() {
-    const randomColorPalette = getRandomObjectKey(colorPalettes);
+    const randomColorPalette = getRandomColorPaletteKey();
     const randomBlockBorder = getRandomObjectKey(blockBorders);
     const randomBlockBorderRadius = getRandomObjectKey(blockBorderRadiuses);
 
